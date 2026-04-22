9c4a4edaa4e09c2a90998bde20988f55c8d28cd6
May Saowapan <my@saowapan.com>
2026-04-22 19:13:35 +0700

    add prod server

--- src/main/java/com/saowapan/expense_tracker_api/config/SecurityConfig.java
+++ src/main/java/com/saowapan/expense_tracker_api/config/SecurityConfig.java
@@ -61,7 +61,11 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration config = new CorsConfiguration();
-        config.setAllowedOrigins(List.of("http://localhost:5173", "http://localhost:5174"));
+        config.setAllowedOrigins(List.of(
+                "http://localhost:5173",
+                "http://localhost:5174",
+                "https://expense-tracker-mu-three-77.vercel.app"
+        ));
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         config.setAllowedHeaders(List.of("*"));
         config.setAllowCredentials(true);
